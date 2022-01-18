0907b324ebd3da2e20b5476ce012a0e3f038cf43
Christopher M. Pierce <chris@chris-pierce.com>
2022-01-18 14:27:02 -0500

    clean up __init__.py

--- beamfit/__init__.py
+++ beamfit/__init__.py
@@ -1,15 +1,6 @@
-#!/usr/bin/env python
-
-################################################################################
-# File: __init__.py
-# Author: Christopher M. Pierce (cmp285@cornell.edu)
-################################################################################
-
-# Import everything from the module
 from .beamfit import fit_supergaussian
 from .fit_param_conversion import get_mu_sigma, get_mu_sigma_std
 from .plotting_and_output import pretty_print_loc_and_size, plot_threshold, plot_residuals, plot_beam_contours
 from .utils import get_image_and_weight
 from gaussufunc import supergaussian
 from .initial_parameter_estimation import fit_gaussian_1d
-
